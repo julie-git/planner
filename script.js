8630c5187113a47aa5962b819e28c5b4f789ef5d
JG <noreply@jg.com>
2019-12-10 12:27:11 -0800

    fixed styling on UI

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 //get today's date
 var today = moment().format('dddd, MMMM D, YYYY');
 console.log(today);
@@ -14,3 +15,20 @@ $("#currentDay").append(today);
 // var dayOfWeekNum = moment().day();
 // var dayOfWeek = moment().day(dayOfWeekNum);
 // console.log(dayOfWeek);
+// renderPast();
+
+// function renderPast(){
+//     console.log("renderpast");
+//     $(".row time-block" ).addClass("past");
+
+// }
+
+$(".saveBtn").click(function() {
+    console.log("clicked save button");
+    console.log("button id" + this.id);
+    // push(this.value)
+    
+});
+
+
+});
