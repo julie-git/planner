0be82c5ca510e42e01dc40325cf3c276439d8b87
JG <noreply@jg.com>
2019-12-09 19:58:58 -0800

    built schedule GUI

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-console.log("julie");
-var myMoment = moment().format();
-console.log(myMoment);
+//get today's date
+var today = moment().format('dddd, MMMM D, YYYY');
+console.log(today);
+
+//put today's date in the header
+$("#currentDay").append(today);
+
+// console.log("moment day of week" + moment().day());
+// console.log("day of month" + moment().date() );
+// console.log("day of year" + moment().dayOfYear());
+// console.log("year" + moment().year());
+// // console.log("all" + moment().year(2019).month(month).date(day));
+
+// var dayOfWeekNum = moment().day();
+// var dayOfWeek = moment().day(dayOfWeekNum);
+// console.log(dayOfWeek);
